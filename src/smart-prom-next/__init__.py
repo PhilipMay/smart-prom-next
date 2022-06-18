b2e359d41d28692beca25aa80f604db5faf75720
Philip May <philip@may.la>
2022-06-18 08:20:28 +0200

    add __init__.py with version

---
+++ src/smart-prom-next/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2022 Philip May
+# This software is distributed under the terms of the MIT license
+# which is available at https://opensource.org/licenses/MIT
+
+"""smart-prom-next main package."""
+
+# Versioning follows the Semantic Versioning Specification https://semver.org/ and
+# PEP 440 -- Version Identification and Dependency Specification: https://www.python.org/dev/peps/pep-0440/  # noqa: E501
+__version__ = "0.0.1rc1"

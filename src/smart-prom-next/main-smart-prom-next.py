c9c9525e21682eb1b0f97db0d08f9cb51df68dd8
Philip May <philip@may.la>
2022-06-18 07:22:53 +0200

    add first version of main

---
+++ src/smart-prom-next/main-smart-prom-next.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2022 Philip May
+# This software is distributed under the terms of the MIT license
+# which is available at https://opensource.org/licenses/MIT
+
+
+def main():
+    print("start smart-prom-next")  # TODO: add version info from __init__.py
+
+
+if __name__ == "__main__":
+    main()

35f8a212bde81e72dd069dc856872e230850e8d4
Philip May <philip@may.la>
2022-06-18 14:33:08 +0200

    add docstrings

--- smart_prom_next/main.py
+++ smart_prom_next/main.py
@@ -2,11 +2,13 @@
 # This software is distributed under the terms of the MIT license
 # which is available at https://opensource.org/licenses/MIT
 
+"""smart-prom-next main module."""
 
 from smart_prom_next import __version__
 
 
 def main():
+    """Main function."""
     print(f"start smart-prom-next version: {__version__}")
 
 

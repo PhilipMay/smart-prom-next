62ec334715eb79589f028ceb78fccbad1332cdd2
Philip May <philip@may.la>
2022-06-19 06:32:52 +0200

    add test for normalize_str

---
+++ tests/test_main.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2022 Philip May
+# This software is distributed under the terms of the MIT license
+# which is available at https://opensource.org/licenses/MIT
+
+
+from smart_prom_next.main import normalize_str
+
+
+def test_normalize_str():
+    input = " ABxy "
+    output = normalize_str(input)
+    assert output == "abxy"

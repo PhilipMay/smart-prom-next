fc701e9fddebc19307bbfcbb6114eff31441e898
Philip May <philip@may.la>
2022-08-08 20:32:41 +0200

    add tests for scrape_metrics_for_device #38

--- tests/test_scrape_metrics_for_device.py
+++ tests/test_scrape_metrics_for_device.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2022 Philip May
 # This software is distributed under the terms of the MIT license
 # which is available at https://opensource.org/licenses/MIT
-
+from math import isclose
 
 from json_fixtures import ATA_FAILED_PAST
 from prometheus_client import REGISTRY
@@ -9,11 +9,7 @@ from prometheus_client import REGISTRY
 from smart_prom_next.smart_prom_next import scrape_metrics_for_device
 
 
-# TODO: integrate this with test_smart_prom_next.py again
-# this is a hack - no other tests must be executed in this script
-# https://stackoverflow.com/questions/73198616/how-do-i-reset-python-runtime-between-pytest-test-functions?utm_source=pocket_mylist
-
-
+# This function must remain the only one in this module.
 def test_scrape_metrics_for_device_ata():
     scrape_metrics_for_device(
         device_name="test_device",
@@ -29,3 +25,33 @@ def test_scrape_metrics_for_device_ata():
     assert "smart_prom_nvme_smart_info" not in metric_names
     assert "smart_prom_scsi_smart_info" not in metric_names
     assert "smart_prom_smart_info" in metric_names
+
+    # test scrape_smart_status
+    smart_prom_smart_info_gauge = REGISTRY.get_sample_value(
+        "smart_prom_smart_info",
+        labels={
+            "attr_id": "3",
+            "attr_name": "spin_up_time",
+            "attr_type": "failed_past",
+            "device": "test_device",
+            "type": "test_type",
+            "model": "SAMSUNG HD204UI",
+            "serial": "S2H7J9FB203635",
+        },
+    )
+    assert smart_prom_smart_info_gauge is not None
+    assert isclose(smart_prom_smart_info_gauge, 1.0)
+
+    # test scrape_temperature
+    smart_prom_temperature_gauge = REGISTRY.get_sample_value(
+        "smart_prom_temperature",
+        labels={
+            'temperature_type': 'current',
+            "device": "test_device",
+            "type": "test_type",
+            "model": "SAMSUNG HD204UI",
+            "serial": "S2H7J9FB203635",
+        },
+    )
+    assert smart_prom_temperature_gauge is not None
+    assert isclose(smart_prom_temperature_gauge, 27.0)
